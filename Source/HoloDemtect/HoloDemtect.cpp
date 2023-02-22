166af6ce6a3511374ab0e09722133695ad1adc9f
Manuel Benavent <mbenavent99@gmail.com>
2023-02-22 09:46:36 +0100

    Project + QRTracking

---
+++ Source/HoloDemtect/HoloDemtect.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HoloDemtect.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HoloDemtect, "HoloDemtect" );
---
+++ Source/HoloDemtect/HoloDemtectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HoloDemtectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOLODEMTECT_API AHoloDemtectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
