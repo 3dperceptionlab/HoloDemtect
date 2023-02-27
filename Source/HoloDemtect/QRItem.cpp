5472bed787486b685b173613c0934516b4bb6ad8
Manuel Benavent <mbenavent99@gmail.com>
2023-02-27 15:32:05 +0100

    Spawning dinamically in HL2 device

--- Source/HoloDemtect/QRItem.cpp
+++ Source/HoloDemtect/QRItem.cpp
@@ -11,10 +11,7 @@ UQRItem::UQRItem()
 void UQRItem::SetParams(FString meshName_, FVector loc, FRotator rot, float_t scale_)
 {
 	meshName = meshName_;
-	//mesh = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), nullptr, (const TCHAR*)*("StaticMesh'" + meshName_ + "'")));
-	//UStaticMesh* mesh_ = LoadObject<UStaticMesh>(nullptr, TEXT("StaticMesh'/Game/YCB/16K/011_banana.011_banana'"), nullptr, LOAD_None, nullptr);
 	
-	//UStaticMesh* mesh_ = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), nullptr, TEXT("StaticMesh'/Game/YCB/16K/017_orange/017_orange'")));
 	UStaticMesh* mesh_ = LoadObject<UStaticMesh>(nullptr, (const TCHAR*)*(meshName_), nullptr, LOAD_None, nullptr);
 	if (mesh_ != nullptr) {
 		mesh = mesh_;
